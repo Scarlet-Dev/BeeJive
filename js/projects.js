fa8316dcda7c429b4d9668bff90cb882ac94ed97
Akane <akane.simpson@gmail.com>
2019-12-13 22:02:00 -0500

    Landing Page compeleted. Created projects, resume and works pages.

---
+++ js/projects.js
@@ -0,0 +1,14 @@
+console.log('test')
+$.getJSON('../data/projects-links.json', function(data){
+    console.log(data);
+})
+
+var commonTags = [
+    "C#",
+    "Web",
+    "MVC",
+    "API",
+    "Angular",
+    "SOAP",
+    "Java"
+]
